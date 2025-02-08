0359dcbe7d0c47eae87a9319cb5bb55b035502b7
dragnel <dragnel@gmail.com>
2025-02-07 21:33:59 -0600

    actualizado

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -57,7 +58,7 @@ def calcular():
     isss_total=isss_total, afp_total=afp_total)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 
